b3125df7b98aa6a25816a583ce99bf1fd7ccb2f2
Sirori <tldus7519@gmail.com>
2023-08-09 11:28:04 +0900

    학습 환경 구성

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// React 모듈을 불러오기
+// ReactDOM 모듈을 불러오기
+
+// React 요소 작성 (React API)
+// React 요소 작성 (with JSX)
+
+// App 함수 컴포넌트 만들기
+
+
+// React.DOM.createRoot() 를 사용해서 DOM 요소를 ReactDOMRoot 객체로 생성
+// ReactDomRoot.render() 메서드로 App을 화면에 표시(렌더링)
